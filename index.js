1ed16f020615fb0edb26c680648d49356b637acd
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-17 21:16:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,18 @@ function showRepositories() {
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
+
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
   req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
   req.send();
 }
