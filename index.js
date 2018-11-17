5bfd4ad3bbf4aab43b9edba88ad80a66c46a6c2f
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-17 21:04:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 function showRepositories() {
-  //this is set to the XMLHttpRequest object that fired the event
   console.log(this.responseText);
+  let repoList = '<ul>';
+  for (var i = 0; i < this.responseText.length; i++) {
+    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
+  }
+  repoList += '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
 }
- 
+
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
