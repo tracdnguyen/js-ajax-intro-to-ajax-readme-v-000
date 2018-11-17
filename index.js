d3b74c1cbda497e56f315e44da7c3aeeffa0cd3d
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-17 21:18:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,21 @@ function getRepositories() {
   req.send();
 }
 
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
+}
+
 function getCommits(el) {
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
